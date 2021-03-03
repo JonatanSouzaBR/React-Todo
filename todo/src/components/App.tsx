93ad3c88f739524ac9a034559c53a694b4cc1f8e
JonatanSouzaBR <jonatan.souza@icloud.com>
2021-03-03 00:32:11 -0300

    Conclusão do projeto

--- todo/src/components/App.tsx
+++ todo/src/components/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import TodoContext from '../contexts/TodoContext';
 import Navbar from './Navbar';
+import TodoList from './TodoList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AddTodo from './AddTodo';
 
 const App = () => {
     return(
-            <div>
-                <h1>First component</h1>
+        <TodoContext>
+            <Router>
                 <Navbar></Navbar>
-            </div>
+                <br />
+                <div className="uk-container">
+                    <Switch>
+                        <Route path="/create">
+                            <AddTodo></AddTodo>
+                        </Route>
+                        <Route path="/">
+                            <h4>Minha lista de tarefas</h4>
+                            <TodoList></TodoList>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </TodoContext>
     );
 }
 
--- todo/src/components/Navbar.tsx
+++ todo/src/components/Navbar.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return(
-        <div>
-            Menu Principal
+        <div className="uk-container">
+            <nav className="uk-navbar">
+                <div className="uk-navbar-left">
+                    <Link to="/" className="uk-navbar-item uk-logo">
+                        <b>WIKI.&nbsp;</b>Dayconnect
+                    </Link>
+                </div>
+
+                <div className="uk-navbar-right">
+                    <ul className="uk-navbar-nav">
+                        <li>
+                            <Link to="/create">
+                                <span uk-icon="icon: plus; ratio: 1.2"></span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 };
